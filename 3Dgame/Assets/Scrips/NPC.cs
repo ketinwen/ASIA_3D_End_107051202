85c17a57703bdb40242ac7e701899bfdfac8e0d1
ketinwen <71679859+ketinwen@users.noreply.github.com>
2020-12-15 16:01:33 +0800

    12/15

---
+++ 3Dgame/Assets/Scrips/NPC.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialoug;
+    [Header("對話內容")]
+    public Text textContent;
+    
+    public bool playerInArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "帥哥哥")
+        {
+            playerInArea = true;
+            Dialoug();
+        }
+    }
+     private void OnTriggerExit(Collider other)
+        {
+            if (other.name == "帥哥哥")
+            {
+                playerInArea = false;
+            }
+        }
+    private void Dialoug()
+     {
+        for (int i = 0; i < data.dialougA.Length; i++)
+        {
+            print(data.dialougA[i]);
+        }
+     }
+    }
