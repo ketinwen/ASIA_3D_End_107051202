19fc67d1ce1d75bbcbcb9793cf80f3d1c619643b
ketinwen <71679859+ketinwen@users.noreply.github.com>
2020-12-22 15:57:40 +0800

    12/22

--- 3Dgame/Assets/Scrips/NPC.cs
+++ 3Dgame/Assets/Scrips/NPC.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class NPC : MonoBehaviour
 {
@@ -11,15 +10,37 @@ public class NPC : MonoBehaviour
     public GameObject dialoug;
     [Header("對話內容")]
     public Text textContent;
-    
+    [Header("對話者名稱")]
+    public Text textName;
+    [Header("對話間隔")]
+    public float interval = 0.2f;
+    /// <summary>
+    /// 玩家是否進入感應區
+    /// </summary>
     public bool playerInArea;
+
+    public enum NPCState
+    {
+        FirstDialoug,Missioning,Finish
+    }
+    public NPCState state = NPCState.FirstDialoug;
+
+    private IEnumerator Test() 
+    {
+        print("嗨！！！");
+        yield return new WaitForSeconds(1.5f);
+        print("嗨，我是1.5秒後");
+        yield return new WaitForSeconds(1.5f);
+
+
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "帥哥哥")
         {
             playerInArea = true;
-            Dialoug();
+            StartCoroutine(Dialoug());
         }
     }
      private void OnTriggerExit(Collider other)
@@ -27,13 +48,41 @@ public class NPC : MonoBehaviour
             if (other.name == "帥哥哥")
             {
                 playerInArea = false;
+            StopDialoug();
             }
         }
-    private void Dialoug()
+    private void StopDialoug()
+    {
+        dialoug.SetActive(false);
+        StopAllCoroutines();
+    }
+    private IEnumerator Dialoug()
      {
-        for (int i = 0; i < data.dialougA.Length; i++)
+        dialoug.SetActive(true);
+
+        textContent.text = "";
+        textName.text =name;
+        
+        string dialougString = data.dialougB;
+
+        switch (state)
         {
-            print(data.dialougA[i]);
+            case NPCState.FirstDialoug:
+                dialougString = data.dialougA;
+                break;
+            case NPCState.Missioning:
+                dialougString = data.dialougB;
+                break;
+            case NPCState.Finish:
+                dialougString = data.dialougC;
+                break;
+            
+        }
+        //字串長度
+        for (int i = 0; i < dialougString.Length; i++)
+        {
+            textContent.text += dialougString[i] + "";
+            yield return new WaitForSeconds(interval);
         }
      }
     }
