483c526fb86bd797a24e9541eb056218892661dd
ketinwen <71679859+ketinwen@users.noreply.github.com>
2020-12-29 15:59:08 +0800

    12/29

---
+++ 3Dgame/Assets/Scrips/Enemy.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;      /// 引用人工智慧
+
+
+public class Enemy : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 50)]
+    public float speed = 4;
+    [Header("停止距離"), Range(0, 50)]
+    public float stopDistance = 2;
+    [Header("攻擊冷卻時間"), Range(0, 50)]
+    public float cd = 2f;
+
+    private Transform player;
+    private NavMeshAgent nav;
+    private Animator ani;
+    /// <summary>
+    /// 計時器
+    /// </summary>
+    private float timer;
+
+    private void Awake()
+    {
+        //取得身上的元件<代理器>
+        nav = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+        //尋找其他遊戲物件("物件名稱").變形元件
+        player = GameObject.Find("帥哥哥").transform;
+        //代理器的速度與停止距離
+        nav.speed = speed;
+        nav.stoppingDistance = stopDistance;
+    }
+    
+    private void Update()
+    {
+        Track();
+        Attack();
+    }
+    private void Attack()
+    {
+        if (nav.remainingDistance < stopDistance)
+        { 
+           //時間累加(一針的時間
+            timer += Time.deltaTime;
+
+            Vector3 pos = player.position;
+            pos.y = transform.position.y;
+
+            transform.LookAt(pos);
+
+            if (timer>=cd)
+            {
+                ani.SetTrigger("攻擊觸發");
+                timer = 0;
+            }
+        }
+    }
+    /// <summary>
+    /// 追蹤
+    /// </summary>
+    private void Track()
+    {
+        nav.SetDestination(player.position);
+
+        ani.SetBool("跑步開關", nav.remainingDistance > stopDistance);
+    }
+
+
+
+
+
+}
